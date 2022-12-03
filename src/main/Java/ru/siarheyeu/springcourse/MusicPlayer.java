e69bd6a3e631a3ecbf737a43857960ff8ee0e9c0
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2022-12-03 23:42:22 +0300

    Dependency Injection 2.2

--- src/main/Java/ru/siarheyeu/springcourse/MusicPlayer.java
+++ src/main/Java/ru/siarheyeu/springcourse/MusicPlayer.java
@@ -3,6 +3,25 @@ package ru.siarheyeu.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     public MusicPlayer(Music music){
         this.music = music;
     }
