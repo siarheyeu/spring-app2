d5655a2d96ed86ec2ff03a654976902964b10df0
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2022-12-04 16:51:43 +0300

    Bean scope

--- src/main/Java/ru/siarheyeu/springcourse/TestSpring.java
+++ src/main/Java/ru/siarheyeu/springcourse/TestSpring.java
@@ -2,6 +2,8 @@ package ru.siarheyeu.springcourse;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml"
@@ -10,11 +12,22 @@ public class TestSpring {
         //Music music = context.getBean("musicBean", Music.class);
 
         //MusicPlayer musicPlayer = new MusicPlayer((music));
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(comparison);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+       // System.out.println(musicPlayer.getName());
+        // System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
