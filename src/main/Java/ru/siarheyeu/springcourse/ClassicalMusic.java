7ae637ab67ea03adce52dbecf7aad5a1ef7d5d53
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2022-12-04 17:28:04 +0300

    Bean Lifecycle 1

--- src/main/Java/ru/siarheyeu/springcourse/ClassicalMusic.java
+++ src/main/Java/ru/siarheyeu/springcourse/ClassicalMusic.java
@@ -1,6 +1,14 @@
 package ru.siarheyeu.springcourse;
 
 public class ClassicalMusic implements Music {
+
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
     @Override
     public String getSong(){
         return "Hungarian Rhapsody";
