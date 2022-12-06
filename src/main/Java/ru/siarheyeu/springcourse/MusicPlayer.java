8540e58b35fd424f420ddbbe18bcaab6baab8c38
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2022-12-06 13:32:15 +0300

    Annotation @Qualifier, Dependency injection

--- src/main/Java/ru/siarheyeu/springcourse/MusicPlayer.java
+++ src/main/Java/ru/siarheyeu/springcourse/MusicPlayer.java
@@ -1,21 +1,24 @@
 package ru.siarheyeu.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockmusic;
 @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockmusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockmusic = rockmusic;
+
+   private Music music1;
+   private Music music2;
+
+   @Autowired
+    public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic(){
-        return "Playing" + classicalMusic.getSong();
+        return "Playing" + music1.getSong() + " " + music2.getSong();
 
     }
 }
